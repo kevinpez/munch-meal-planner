3fa9ce5f4e2960d06ae2b2c498a8dced131e95f1
Kevin Pesenecker <kevinpez@gmail.com>
2024-11-02 09:56:06 -0600

    heroku db imgration

--- migrations/versions/add_image_url_column.py
+++ migrations/versions/add_image_url_column.py
@@ -1,16 +1,15 @@
-"""Add image_url to Recipe model
+"""add image_url to recipe
 
-Revision ID: add_image_url_column
-Create Date: 2024-11-02
+Revision ID: (will be auto-generated)
+Revises: b381b38cbac6
+Create Date: (will be auto-generated)
+
 """
 from alembic import op
 import sqlalchemy as sa
-
-revision = 'add_image_url_column'
-down_revision = None
 
 def upgrade():
     op.add_column('recipe', sa.Column('image_url', sa.String(length=500), nullable=True))
 
 def downgrade():
-    op.drop_column('recipe', 'image_url') 
+    op.drop_column('recipe', 'image_url')
