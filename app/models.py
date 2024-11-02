3aca10c7d52178bfe30fa9426409b133e8c0de9d
Kevin Pesenecker <kevinpez@gmail.com>
2024-11-01 23:07:42 -0600

    migrate heroku db

--- app/models.py
+++ app/models.py
@@ -44,7 +44,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False, index=True)
     email = db.Column(db.String(120), unique=True, nullable=False, index=True)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
     recipes = db.relationship('Recipe', backref='author', lazy='dynamic')
 
     @validates('username')
